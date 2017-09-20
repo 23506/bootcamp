3d5e3cbe1cad6193811cfa55dea4c5bb52f7313e
23506 <23506@ma-web.nl>
2017-09-20 02:53:27 +0200

    variable declare
    I created multiple variables

---
+++ bootcamp.js
@@ -0,0 +1,13 @@
+var firstVar;
+
+var secondVar, thirdVar, FourthVar, fifthVar, sixthVar, seventhVar, eighthVar,
+ninthVar, tenthVar, eleventhVar, twelfthVar, thirteenthVar, fifteenthVar, SixteenthVar;
+
+secondVar = "b";
+FourthVar = 2;
+fifthVar= "hello";
+sixthVar = "a";
+seventhVar = true;
+eighthVar = 7,23;
+ninthVar, tenthVar, eleventhVar, twelfthVar, thirteenthVar, fifteenthVar, SixteenthVar;
+
